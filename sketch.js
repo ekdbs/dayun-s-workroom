9b3add125db3be1ee0f66921afbd93f87640f828
ekdbs <94090453+ekdbs@users.noreply.github.com>
2022-11-25 14:14:42 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,41 @@
-let mySound;
+// let musicFile;
+// function setup()
+// {
+// soundFormats('mp3','ogg');
+// musicFile = loadSound('intention',loadMusic);
+// // musicFile.play();
+// }
+
+// function loadMusic()
+// {
+// musicFile.play(); 
+// }
+
+var song
+var button;
+
 function setup(){
-    soundFormats('mp3','ogg');
-    musicFile = loadSound('music', loadMusic);
+    createCanvas(640, 480);
+    song = loadSound('intention.mp3', loaded);
+    button = createButton('play');
+    button.mousePressed(togglePlaying);
+}
+function togglePlaying(){
+    if(!song.isPlaying()){
+        song.play();
+        song.setVolume(0.3);
+        button.html('pause');
+    } else{
+        song.stop();
+        button.html('play');
+    }
 }
 
-function loadMusic(){
-    musicfile.play()
+function loaded(){
+    console.log("It's working");
+}
+
+function draw(){
+    fill('pink')
+    ellipse(50, 50, 100, 100);
 }
